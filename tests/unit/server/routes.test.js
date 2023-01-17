704425bbc402082c8426f0727588410c0cb26a7c
pedrocarr <eng.pedro.amb@gmail.com>
2023-01-17 11:54:51 -0300

    implement routes todo tests

--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -1,9 +1,19 @@
-import { jest, expect, test, describe } from '@jest/globals'
+import { jest, expect, test, describe, beforeEach } from '@jest/globals'
+import config from '../../../server/config.js'
 
+const {
+  pages
+} = config
 
-describe('#Routes', () => {
-  test('should do something', () => {
-    expect(true).toBeTruthy();
-  });
-}
-);
+describe('#Routes - test site for api response', () => {
+    beforeEach(() => {
+      jest.resetAllMocks()
+      jest.clearAllMocks() // clear all mocks
+  })
+
+test.todo('GET / - should redirect to home page')
+test.todo(`GET /home - should response with ${pages.homeHTML} file stream`)
+test.todo(`GET /controller - should response with ${pages.controllerHTML} file stream`)
+
+})
+
